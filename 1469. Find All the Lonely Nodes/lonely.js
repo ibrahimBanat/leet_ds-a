269564a39bfd0a6c3b07834717a369d7410473a1
IbrahimBanat <ibrahim.banat.97@gmail.com>
2021-07-07 21:49:12 +0300

    adding the correct sol for loenly Nodes

--- 1469. Find All the Lonely Nodes/lonely.js
+++ 1469. Find All the Lonely Nodes/lonely.js
@@ -13,11 +13,18 @@ function getLonely(root) {
   let loneyNodes = [];
   if (root === null) retrun;
   function traverse(node) {
-    if (node.left != null && node.right === null) {
-      lonelyNodes.push(node.left);
+    if (node.left !== null) {
+      if (node.right === null) {
+        lonelyNodes.push(node.left.val);
+      }
+      traverse(node.left);
     }
-    if (node.right !== null && node.left === null) {
-      lonelyNodes.push(node.right);
+
+    if (node.right !== null) {
+      if (node.left === null) {
+        loneyNodes.push(node.right);
+      }
+      traverse(node.right);
     }
   }
 
