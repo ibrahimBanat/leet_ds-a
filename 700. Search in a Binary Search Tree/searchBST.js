316ef657905d4fbf6726c3f5afe1c062d098a872
IbrahimBanat <ibrahim.banat.97@gmail.com>
2021-07-08 18:23:23 +0300

    solution for searching in BST

---
+++ 700. Search in a Binary Search Tree/searchBST.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ 
+ */
+var searchBST = function (root, val) {
+  function traverse(node) {
+    if (!node) return null;
+    if (node.val === val) return node;
+    return node.val > val ? traverse(node.left) : traverse(node.right);
+  }
+  return traverse(root);
+};
