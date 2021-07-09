437857eef473abc2c89aa05b83289439f2ab6674
IbrahimBanat <ibrahim.banat.97@gmail.com>
2021-07-09 20:09:30 +0300

    solution for the inOrder traversal of binary tree

---
+++ 94. Binary Tree Inorder Traversal/README.md
@@ -0,0 +1,16 @@
+# 94. Binary Tree Inorder Traversal
+
+`level: Easy`.
+
+[Link](https://leetcode.com/problems/binary-tree-inorder-traversal/)
+
+## Problem Domain
+
+Given the `root` of a binary tree, return the _inorder traversal of its nodes_' values.
+
+![](https://assets.leetcode.com/uploads/2020/09/15/inorder_1.jpg)
+
+```
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+```
---
+++ 94. Binary Tree Inorder Traversal/inOrderTrav.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let array = [];
+  if (root === null) return array;
+
+  function traverse(node) {
+    node.left && traverse(node.left);
+    array.push(node.val);
+    node.right && traverse(node.right);
+  }
+  traverse(root);
+  return array;
+};
